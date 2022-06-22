2bb70844cebbdb89c82a47faf2f6d99004a33a56
arael3 <34441615+arael3@users.noreply.github.com>
2022-06-22 23:07:50 +0200

    Timer UI added

---
+++ the boxex are lava - unity project/Assets/Scripts/Timer.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    TextMeshProUGUI timeText;
+    float time;
+    int timeInt;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeText = GetComponent<TextMeshProUGUI>();
+        time = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        timeInt = (int)time;
+        timeText.text = timeInt.ToString();
+    }
+}
