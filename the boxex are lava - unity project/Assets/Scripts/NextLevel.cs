646de53f910db6b65dad402bf950b60bba6aad17
arael3 <34441615+arael3@users.noreply.github.com>
2022-06-24 22:56:52 +0200

    Coins For Time sound added

--- the boxex are lava - unity project/Assets/Scripts/NextLevel.cs
+++ the boxex are lava - unity project/Assets/Scripts/NextLevel.cs
@@ -6,15 +6,19 @@ public class NextLevel : MonoBehaviour
 {
     Points points;
 
+    SoundController soundController;
+
     void Start()
     {
         points = GameObject.Find("Points Text").GetComponent<Points>();
+        soundController = GameObject.FindGameObjectWithTag("SoundController").GetComponent<SoundController>();
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
+            soundController.PlaySound("CoinsForTime");
             points.addPointsForTime = true;
 
             //GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>().StartCoroutine("NextLevel");
